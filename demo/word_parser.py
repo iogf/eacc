591ef84dade7acd51c51a1d8c866c48093c140a1
Iury O. G. Figueiredo <last.src@gmail.com>
2020-05-06 02:00:17 +0000

    Fixing example.

--- demo/word_parser.py
+++ demo/word_parser.py
@@ -1,7 +1,7 @@
 """
 """
 
-from eacc.eacc import Eacc, Rule, Grammar, Struct
+from eacc.eacc import Eacc, Rule, Grammar
 from eacc.lexer import XSpec, Lexer, LexTok
 from eacc.token import Blank, Word, TokVal, Sof, Eof
 
@@ -12,14 +12,12 @@ class WordTokens(XSpec):
     root = [t_word, t_blank]
 
 class WordGrammar(Grammar):
-    struct     = Struct()
     r_phrase0  = Rule(TokVal('alpha'), TokVal('beta'))
     r_phrase1  = Rule(TokVal('gamma'), TokVal('zeta'))
     r_sof      = Rule(Sof)
     r_eof      = Rule(Eof)
 
-    struct.add(r_phrase1, r_phrase0, r_sof, r_eof)
-    root = [struct]
+    root = [r_phrase1, r_phrase0, r_sof, r_eof]
 
 data = 'alpha beta gamma zeta' 
 lexer = Lexer(WordTokens)
